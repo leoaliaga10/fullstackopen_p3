d1ed316cb3161698685a68fd6ffcbb266d1b31f7
Leoncio Aliaga Saldana <leo.as10@gmail.com>
2025-05-13 01:41:12 -0500

    Commit for exercise 3.13 and 3.14

--- exercise3_9/backend/index.js
+++ exercise3_9/backend/index.js
@@ -1,34 +1,14 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import People from "./models/people.js";
 
 const app = express();
 app.use(express.static("dist"));
 app.use(cors());
 app.use(express.json());
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+let persons = [];
 
 //Custom token for the request body
 morgan.token("body", (req) => {
@@ -44,42 +24,49 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-//app.use(morgan("tiny")); // Use the middleware morgan with configuration 'tiny'
-
-//exercise 3.1
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  People.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
-//exercise 3.2
 app.get("/info", (request, response) => {
-  const now = new Date();
-  response.send(
-    `<p>Phonebook has info ${
-      persons.length
-    } for people</p><br/>${now.toString()}`
-  );
+  People.find({}).then((persons) => {
+    const now = new Date();
+    response.send(
+      `<p>Phonebook has info ${persons.length} for people</p><br/>${now}</p>`
+    );
+  });
 });
 
-//exercise 3.3
 app.get("/api/persons/:id", (request, response) => {
   const id = request.params.id;
-  const person = persons.find((person) => person.id == id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  //  const person = People.findById(id);
+
+  People.findById(id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error)); // manejar errores como id mal formateado
 });
 
-//exercise 3.4
 app.delete("/api/persons/:id", (request, response) => {
   const id = request.params.id;
-  persons = persons.filter((person) => person.id != id);
-  response.status(204).end();
+  People.findByIdAndDelete(id)
+    .then((person) => {
+      if (person) {
+        response.status(204).end();
+      } else {
+        response.status(404).json({ error: "person not found" });
+      }
+    })
+    .catch((error) => next(error)); // Maneja errores como IDs malformados
 });
 
-//exercise 3.5
 const generateId = () => {
   const maxId = Math.floor(Math.random() * 1000);
   return maxId;
@@ -106,18 +93,18 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  const person = {
+  const person = new People({
     name: body.name,
     number: body.number,
     id: generateId(),
-  };
+  });
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ exercise3_9/backend/models/people.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+
+mongoose.set("strictQuery", false);
+
+const url = process.env.MONGODB_URI;
+
+console.log("connecting to", url);
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+const peopleSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+});
+
+peopleSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+//module.exports = mongoose.model("Person", personSchema);
+export default mongoose.model("People", peopleSchema, "people");
